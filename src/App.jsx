8378837f3340f3e579005f18d70ec6ea3b440024
Azizbek Shoyoqubov <a.shoyoqubov@evos.uz>
2023-09-10 16:03:28 +0500

    add  admin pagae and home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="App">
+        <h1>React</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/admin" element={<Home />} />
+        </Routes>
+      </div>
+    </>
+  );
 }
 
 export default App;
